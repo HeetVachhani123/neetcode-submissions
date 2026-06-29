f70d10ceafe9ca978c321352aae63639fc7292e6
Vachhani Heet Rajubhai <vachhaniheet1504@gmail.com>
2026-06-29 12:40:26 +0530

    Add: find-common-characters - submission-0

---
+++ Data Structures & Algorithms/find-common-characters/submission-0.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        // Initialize the tracking array with maximum possible values
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+        
+        for (String word : words) {
+            int[] charFreq = new int[26];
+            // Count frequencies of characters in the current word
+            for (int i = 0; i < word.length(); i++) {
+                charFreq[word.charAt(i) - 'a']++;
+            }
+            
+            // Keep the minimum frequency found for each character across all words
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charFreq[i]);
+            }
+        }
+        
+        List<String> result = new ArrayList<>();
+        // Collect characters that appeared in all words
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i] > 0) {
+                result.add(String.valueOf((char) (i + 'a')));
+                minFreq[i]--;
+            }
+        }
+        
+        return result;
+    }
+}
