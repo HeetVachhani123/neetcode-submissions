8a147ba8a263003ee47f6da391e06d52deeec2ea
Vachhani Heet Rajubhai <vachhaniheet1504@gmail.com>
2026-07-09 17:38:23 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        // dp[i][j] stores the minimum edit distance between word1[0...i-1] and word2[0...j-1]
+        int[][] dp = new int[m + 1][n + 1];
+        
+        // Base cases: transforming a string to/from an empty string
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        
+        // Build the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    // Characters match, no new operation needed
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // Consider all three operations and take the minimum
+                    int replace = dp[i - 1][j - 1];
+                    int delete = dp[i - 1][j];
+                    int insert = dp[i][j - 1];
+                    
+                    dp[i][j] = 1 + Math.min(replace, Math.min(delete, insert));
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
