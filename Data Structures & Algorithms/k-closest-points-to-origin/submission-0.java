44ca10c8b9d9b7aa393281b72badb7d26ab73827
Vachhani Heet Rajubhai <vachhaniheet1504@gmail.com>
2026-08-04 10:20:18 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        java.util.Arrays.sort(points, (a, b) -> 
+            (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])
+        );
+
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = points[i];
+        }
+
+        return result;
+    }
+}
