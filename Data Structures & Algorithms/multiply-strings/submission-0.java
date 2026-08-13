3390d346fb8025f0515e0891a15d278fb63849ba
Vachhani Heet Rajubhai <vachhaniheet1504@gmail.com>
2026-08-13 16:58:32 +0530

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int x = Integer.valueOf(num1);
+        int y = Integer.valueOf(num2);
+        int z = x*y;
+        String ans = String.valueOf(z);
+        return ans;
+    }
+}
